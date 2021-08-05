adc7df95fa195399d1358a14f35381913c751d9d
joshuamr17 <joshuamurillo87@gmail.com>
2021-08-04 21:51:09 -0600

    documenta imagen producto

--- ProyectoFinal/API.W/Models/Producto.cs
+++ ProyectoFinal/API.W/Models/Producto.cs
@@ -13,7 +13,7 @@ namespace API.W.Models
         public int CodigoBarras { get; set; }
         public DateTime FechaIngreso { get; set; }
         public string NombreProducto { get; set; }
-        public byte[] ImagenProducto { get; set; }
+        //public byte[] ImagenProducto { get; set; }
         public int CantidadProducto { get; set; }
         public int PrecioUnitario { get; set; }
         public int PrecioVenta { get; set; }
